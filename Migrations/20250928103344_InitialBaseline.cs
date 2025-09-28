b09c72ff30321e4babc4a60a9d6a992e0df3428e
FirstEr <ifurersoy@gmail.com>
2025-09-28 13:39:00 +0300

    xzc

--- Migrations/20250928103344_InitialBaseline.cs
+++ Migrations/20250928103344_InitialBaseline.cs
@@ -10,13 +10,24 @@ namespace BeePM.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Username", "FullName", "Role" },
+                values: new object[,]
+                {
+                    { 1, "employee1", "User 1 Çalışan", "Employee" },
+                    { 2, "manager1", "User 2 Müdür", "Manager" },
+                    { 3, "board1", "User 3 Yönetim Kurulu", "Board" },
+                    { 4, "admin", "Admin Kullanıcı", "Admin" }
+                });
         }
 
-        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 1);
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 2);
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 3);
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 4);
         }
     }
 }
