d768b67791baf74c193522edb14a2a530f7cb07d
FirstEr <ifurersoy@gmail.com>
2025-09-28 13:42:59 +0300

    a

--- Models/BeePMDbContext.cs
+++ Models/BeePMDbContext.cs
@@ -16,15 +16,15 @@ namespace BeePM.Models
         public DbSet<ApprovalFlow> ApprovalFlows { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            //base.OnModelCreating(modelBuilder);
 
-            // ✅ Seed Users
-            modelBuilder.Entity<User>().HasData(
-                new User { Id = 1, Username = "employee1", FullName = "User 1 Çalışan", Role = "Employee" },
-                new User { Id = 2, Username = "manager1", FullName = "User 2 Müdür", Role = "Manager" },
-                new User { Id = 3, Username = "board1", FullName = "User 3 Yönetim Kurulu", Role = "Board" },
-                new User { Id = 4, Username = "admin", FullName = "Admin Kullanıcı", Role = "Admin" }
-            );
+            //// ✅ Seed Users
+            //modelBuilder.Entity<User>().HasData(
+            //    new User { Id = 1, Username = "employee1", FullName = "User 1 Çalışan", Role = "Employee" },
+            //    new User { Id = 2, Username = "manager1", FullName = "User 2 Müdür", Role = "Manager" },
+            //    new User { Id = 3, Username = "board1", FullName = "User 3 Yönetim Kurulu", Role = "Board" },
+            //    new User { Id = 4, Username = "admin", FullName = "Admin Kullanıcı", Role = "Admin" }
+            //);
         }
     }
 }
